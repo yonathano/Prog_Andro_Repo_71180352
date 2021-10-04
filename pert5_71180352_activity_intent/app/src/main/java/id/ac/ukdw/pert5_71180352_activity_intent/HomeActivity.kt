163e6dbb28be42a3bafb92285b0e396b51a8aaed
yonathano <71685928+yonathano@users.noreply.github.com>
2021-10-05 00:22:05 +0700

    menambah intent

--- pert5_71180352_activity_intent/app/src/main/java/id/ac/ukdw/pert5_71180352_activity_intent/HomeActivity.kt
+++ pert5_71180352_activity_intent/app/src/main/java/id/ac/ukdw/pert5_71180352_activity_intent/HomeActivity.kt
@@ -2,8 +2,17 @@ package id.ac.ukdw.pert5_71180352_activity_intent
 
 import android.os.Bundle
 import  android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
+        val Uname = intent.getStringExtra("Username")
+
+        val txtHasil = findViewById<TextView>(R.id.textHasil)
+        txtHasil.text = "Selamat Datang ${Uname}"
+    }
 }
--- pert5_71180352_activity_intent/app/src/main/java/id/ac/ukdw/pert5_71180352_activity_intent/MainActivity.kt
+++ pert5_71180352_activity_intent/app/src/main/java/id/ac/ukdw/pert5_71180352_activity_intent/MainActivity.kt
@@ -1,11 +1,25 @@
 package id.ac.ukdw.pert5_71180352_activity_intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtUname = findViewById<EditText>(R.id.edtUname)
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(){
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("Username", edtUname.text.toString())
+            startActivity(intent)
+        }
     }
 }
