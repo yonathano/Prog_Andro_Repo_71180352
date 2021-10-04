0dbed3dee70f4d38764ed36642bf5bfd95ead576
yonathano <71685928+yonathano@users.noreply.github.com>
2021-10-04 12:32:33 +0700

    login dan home

---
+++ pert5_71180352_activity_intent/app/src/main/java/id/ac/ukdw/pert5_71180352_activity_intent/HomeActivity.kt
@@ -0,0 +1,9 @@
+package id.ac.ukdw.pert5_71180352_activity_intent
+
+import android.os.Bundle
+import  android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity: AppCompatActivity() {
+
+}
