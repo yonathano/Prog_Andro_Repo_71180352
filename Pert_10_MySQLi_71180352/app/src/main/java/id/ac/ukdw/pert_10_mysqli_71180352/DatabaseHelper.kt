53178d19984e376d83336e978f542a0897cdae6b
yonathano <71685928+yonathano@users.noreply.github.com>
2021-11-16 08:24:00 +0700

    Create DatabaseHelper.kt

---
+++ Pert_10_MySQLi_71180352/app/src/main/java/id/ac/ukdw/pert_10_mysqli_71180352/DatabaseHelper.kt
@@ -0,0 +1,20 @@
+package id.ac.ukdw.pert_10_mysqli_71180352
+
+
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+     override fun onCreate(db: SQLiteDatabase) {
+         db.execSQL(DatabaseContract.Penduduk.SQL_CREATE_TABLE)
+         }
+     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+         db.execSQL(DatabaseContract.Penduduk.SQL_DELETE_TABLE)
+         onCreate(db)
+         }
+     override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+         onUpgrade(db, oldVersion, newVersion)
+         }
+     companion object {
+         
+         const val DATABASE_VERSION = 1
+         const val DATABASE_NAME = "mydatabase.db"
+         }
+     }
