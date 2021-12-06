5074a0b933a0bd2041fbffbe6702bbeaf2a7ecea
yonathano <71685928+yonathano@users.noreply.github.com>
2021-12-07 06:46:48 +0700

    commit

---
+++ Pertemuan13_JavaBinding/app/src/main/java/id/ac/ukdw/pertemuan13_javabinding/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ukdw.pertemuan13_javabinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
